bd5ea78932594994c11c304bff87ee3cb2d01bc2
BRSADT <brendasamant@hotmail.com>
2016-10-11 16:16:41 -0500

    emm holi

---
+++ ProyectoDW.php
@@ -0,0 +1,78 @@
+<html>
+<head>
+<title>
+*Inserte_nombre_Ingenioso
+</title>
+  <link rel="stylesheet" href="Estilo_proyecto.css">
+  <meta charset="UTF-8">
+</head>
+<body>
+
+
+
+
+<header>
+
+
+
+<div id="division">
+<span class=""><INPUT type="text" placeholder="Contraseña"></span>
+<span class=""><input type="email" placeholder="Correo Electronico"></span>
+</div>
+
+<div class="grad-nex">
+</div>
+
+<div id="logo">
+<img class="ocultar-pequeño " src="logo.png" alt="fruta">			
+<p class="ocultar-pequeño">CB-<strong>company</strong></p>
+</div>
+
+	<nav>
+	
+
+	
+	<ul id="menu">
+			<li class="dropdown"><a  href="ActvLudicas.php" class="hvr-underline-from-right" >Inicio</a>
+		
+<ul class="dropdown-content">
+<li><a href"#"> Mision </a> </li>
+<li><a href"#"> vision </a> </li>
+<li><a href"#"> Valores </a> </li>
+</ul>
+</li>
+
+		<li class="dropdown"><a href="#" class="hvr-underline-from-right">Tutoriales</a>
+<ul class="dropdown-content">
+<li><a href"#"> ¿Como usar el leap motion? </a> 
+<li><a href"#"> Conceptos </a> 
+<li><a href"#"> Puntaje </a> 
+</ul>			
+</li>
+			<li ><a href="cursos.php" class="hvr-underline-from-right">Cursos</a></li>
+			<li class="dropdown"><a href="#" class="hvr-underline-from-right">Preguntame algo</a>
+			<ul class="dropdown-content">
+			<li><a href="ventas.php" class="boton">Comprar</a>
+			</ul>			
+</li>
+			
+			<li><a href="#" class="hvr-underline-from-right">Contacto</a></li>
+			</ul>			
+</li>
+		</ul>
+		
+	</nav>
+
+
+
+
+</header>
+
+
+
+
+
+
+
+</body>
+</html>
