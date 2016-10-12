22d67750a993e2ec6c25b390e3653dabf599fab7
BRSADT <brendasamant@hotmail.com>
2016-10-12 06:20:36 -0500

    desperte \:V/
    cambio opciones menu

--- ProyectoDW.php
+++ ProyectoDW.php
@@ -16,8 +16,8 @@
 
 
 <div id="division">
-<span class=""><INPUT type="text" placeholder="Contraseña"></span>
 <span class=""><input type="email" placeholder="Correo Electronico"></span>
+<span class=""><INPUT type="text" placeholder="Contraseña"></span>
 </div>
 
 <div class="grad-nex">
@@ -34,28 +34,28 @@
 			<li class="dropdown"><a  href="ActvLudicas.php" class="hvr-underline-from-right" >Inicio</a>
 
 <ul class="dropdown-content">
-<li><a href"#"> Mision </a> </li>
-<li><a href"#"> vision </a> </li>
-<li><a href"#"> Valores </a> </li>
+
 </ul>
 </li>
 
-		<li class="dropdown"><a href="#" class="hvr-underline-from-right">Tutoriales</a>
+		<li class="dropdown"><a href="#" class="hvr-underline-from-right">Productos</a>
 <ul class="dropdown-content">
-<li><a href"#"> ¿Como usar el leap motion? </a>
-<li><a href"#"> Conceptos </a>
-<li><a href"#"> Puntaje </a>
+<li><a href"#"> Frutas </a> 
+<li><a href"#"> Verduras </a> 
+<li><a href"#"> Electronica </a>
+<li><a href"#"> Canasta basica </a> 
+<li><a href"#"> Ofertas </a>
 </ul>
 </li>
-			<li ><a href="cursos.php" class="hvr-underline-from-right">Cursos</a></li>
-			<li class="dropdown"><a href="#" class="hvr-underline-from-right">Preguntame algo</a>
+			<li ><a href="cursos.php" class="hvr-underline-from-right">Oficinas</a></li>
+			<li class="dropdown"><a href="#" class="hvr-underline-from-right">Compras</a>
 			<ul class="dropdown-content">
 			<li><a href="ventas.php" class="boton">Comprar</a>
+				<li><a href="ventas.php" class="boton">Ver carrito</a>
 			</ul>
 </li>
 
-			<li><a href="#" class="hvr-underline-from-right">Contacto</a></li>
-			</ul>
+			
 </li>
 		</ul>
 
