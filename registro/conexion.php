14f85c3a7e028354a6e676e17997223424142e97
BRSADT <brendasamant@hotmail.com>
2016-10-16 20:25:40 -0500

    Registro sin verificar existencia

--- registro/conexion.php
+++ registro/conexion.php
@@ -1,5 +1,7 @@
 <?php
-function conectar()
+
+class BaseDeDatos {
+function __construct()
 {
 
 
@@ -12,10 +14,43 @@ if (!$enlace) {
     exit;
 }
 
-echo "Éxito: Se realizó una conexión apropiada a MySQL! La base de datos mi_bd es genial." . PHP_EOL;
-echo "Información del host: " . mysqli_get_host_info($enlace) . PHP_EOL;
-
 
 }
 
+
+function generaPaises()
+{
+	include 'conexion.php';
+$enlace = mysqli_connect("localhost", "root", "", "base_proyecto_dw");
+	$consulta=mysqli_query($enlace,"SELECT id, nombre FROM estados");
+mysqli_query($enlace,"SET NAMES 'utf8'");
+
+
+	
+	echo "<select name='paises' id='paises' onChange='cargaContenido(this.id)'>";
+	echo "<option value='0'>Estado </option>";
+
+	while($registro=mysqli_fetch_row($consulta))
+	{
+		echo "<option value='".$registro[0]."'>".$registro[1]."</option>";
+	
+	}
+	
+	echo "</select>";
+	
+}
+
+
+function insertarUsuario($Pass,$Usuario,$Nombre,$Apellido,$Email,$Admin,$estado,$municipio)
+{
+	$insert=mysqli_query($enlace,"INSERT INTO persona (Pass,Usuario,Nombre,Apellido,Email,Admin,estado,municipio) VALUES ($Edad,$Pass,$Usuario,$Nombre,$Apellido,$Email,$Admin,$estado,$municipio)");
+mysqli_query($enlace,"SET NAMES 'utf8'");
+
+
+
+	
+}
+}
 ?>
+
+
--- registro/envio_registro.php
+++ registro/envio_registro.php
@@ -4,46 +4,94 @@
 <title>AJAX, Ejemplos: Combos (select) dependientes, codigo fuente - ejemplo</title>
 <link rel="stylesheet" type="text/css" href="select_dependientes.css">
 <script type="text/javascript" src="select_dependientes.js"></script>
+<?php include 'conexion.php'; ?>
 </head>
 
 <body>
 
 <?php
 
-if($_POST['paises']==0){
-echo "no elegiste estado >:v";
+
+
+function insertarUsuario($Pass,$Usuario,$Nombre,$Apellido,$Email,$Admin,$paises,$estados,$calle,$tarjeta,$TarjetaN)
+{
+
+$enlace = mysqli_connect("localhost", "root", "", "base_proyecto_dw");
+	
+	
+$resultado = mysqli_query($enlace,"SELECT nombre FROM estados where estados.id='$paises'") ;
+ while ($row = mysqli_fetch_assoc($resultado)) {
+        echo  $row["nombre"];
+		$estado=$row["nombre"];
+    }
+
+	$resultad = mysqli_query($enlace,"SELECT nombre FROM municipios where municipios.id='$estados'") ;
+ while ($row = mysqli_fetch_assoc($resultad)) {
+        echo  $row["nombre"];
+		$municipios=$row["nombre"];
+    }
+	
+
+$enlace = mysqli_connect("localhost", "root", "", "base_proyecto_dw");
+$STATEMENT="INSERT INTO persona (Pass,Usuario,Nombre,Apellido,Email,Admin,estado,municipio) VALUES ('$Pass','$Usuario','$Nombre','$Apellido','$Email','$Admin','$estado','$municipios')";
+mysqli_query($enlace,$STATEMENT);
+mysqli_query($enlace,"SET NAMES 'utf8'");
+
+
 }
-else {
+
+?>
+<?php
+
+
+
 if (isset($_POST["email"])) {
+$usuario=$_POST["usuario"];
 echo $_POST["usuario"]; //email
 echo " ";
+$pass=$_POST['pass'];
 echo $_POST['pass']; //id estado
 echo " ";
+$pass2=$_POST['pass2'];
 echo $_POST['pass2']; //id estado
 echo " ";
+$nombre=$_POST['nombre'];
 echo $_POST['nombre']; //id estado
 echo " ";
+$apellido= $_POST['apellido']; 
 echo $_POST['apellido']; //id estado
 echo " ";
-
+$email=$_POST["email"];
 echo $_POST["email"]; //email
 echo " ";
+$paises=$_POST['paises'];
 echo $_POST['paises']; //id estado
 echo " ";
-
+$estados=$_POST["estados"];
 echo $_POST["estados"]; //id municipio
 echo " ";
+$calle=$_POST['calle'];
 echo $_POST['calle']; //calle
 echo " ";
-
+$tarjeta=$_POST['tarjeta'];
 echo $_POST['tarjeta']; //id estado
 echo " ";
+
+$TarjetaN=$_POST['TarjetaN']; 
 echo $_POST['TarjetaN']; //id estado
-}else{  
-    echo "N0, mail is not set";
+echo "\n   ";
+insertarUsuario($pass,$usuario,$nombre,$apellido,$email,0,$paises,$estados,$calle,$tarjeta,$TarjetaN);
+
+
+
+
+
+
+
+
 }
 
 
-}?>
+?>
 </body>
 </html>
