dbe72268a7a3590623fd58a46a7b1df9454dc087
BRSADT <brendasamant@hotmail.com>
2016-10-17 22:10:12 -0500

    sigue sin funcar acentos :(

--- registro/Registro.php
+++ registro/Registro.php
@@ -1,5 +1,5 @@
 
-<?php header('Content-Type: text/html; charset=UTF-7'); ?>
+<?php header("Content-Type: text/html;charset=utf-8"); ?>
 <?php
 
 function generaPaises()
@@ -23,17 +23,16 @@ mysqli_query($enlace,"SET NAMES 'utf8'");
 	
 }
 ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 
-<html lang="es">
+
+
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<script type="text/javascript" src="validacion_registro/validacionJ.js"></script>
 
-<title>AJAX, Ejemplos: Combos (select) dependientes, codigo fuente - ejemplo</title>
+<script type="text/javascript" src="validacion_registro/validacionJ.js"></script>
+<title>TACOrporation</title>
 <link rel="stylesheet" type="text/css" href="select_dependientes.css">
 <script type="text/javascript" src="select_dependientes.js"></script>
-<meta http-equiv="Content-Type" content="text/html"; charset=utf-8"/> 
+
 
 
 	
@@ -80,7 +79,7 @@ mysqli_query($enlace,"SET NAMES 'utf8'");
 						<option value="3">Master Card</option>
 					</select>
 
-			<input type="text" name="TarjetaN" id="TarjetaN" placeholder="Numero Tarjeta" pattern="^(?:5[1-9][0-9]{2}|222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720)[0-9]{12}$|^3[47][0-9]{13}$|^4[0-9]{12}(?:[0-9]{3})?$" required> <br><br>				
+			<input type="text" name="TarjetaN" id="TarjetaN" placeholder="Numero Tarjeta" pattern="^(?:5[1-9][0-9]{2}|222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720)[0-9]{12}$|^3[47][0-9]{13}$|^4[0-9]{12}(?:[0-9]{3})?$" required> 				
 <input type="text" class="text" id="Titular" name="Titular" tabindex="2" placeholder="Titular" maxlength="30" required><br><br>			
 			<input type="submit" value="Enviar">
 			</form>
