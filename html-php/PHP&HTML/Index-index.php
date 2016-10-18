547b04e0173899e136158162ac8793a1955a2f07
BRSADT <brendasamant@hotmail.com>
2016-10-18 09:53:18 -0500

    123

--- html-php/PHP&HTML/Index-index.php
+++ html-php/PHP&HTML/Index-index.php
@@ -1,13 +1,26 @@
+
+<?php
+header("Content-Type: text/html;charset=utf-8"); 
+include("../../html-php/funciones/GeneraEstado.php")
+ ?>
+
+<br>
+ 
+ 
 <!DOCTYPE html>
 <ntml lang="es">
 
   <head>
     <Title>Inicio</title>
+	
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <link rel="stylesheet" href="../css/bootstrap.min.css">
     <link rel="stylesheet" href="../css/estilos.css">
         <link rel="stylesheet" href="../css/fuentes.css">
+		<script type="text/javascript" src="../../html-php/js/select_dependientes.js"></script>
+
+		<script type="text/javascript" src="../../html-php/js/validacionJ.js"></script>
   </head>
 <body>
 
@@ -24,26 +37,25 @@
           <span class="icon-bar"></span>
         </button>
         <a href="#" class="navbar-brand">TACOrporation</a>
-
-
-
+        
+		
+		
 		<?php
 			session_start();?>
-			<?php 	if(!isset($_SESSION['name'])):
-
+			<?php 	if(!isset($_SESSION['name'])): 
+			
 			?>
 		<a href="#" class="navbar-brand" data-toggle="modal" data-target="#login-modal">Iniciar sesion</a>
         <a href="#" class="navbar-brand" data-toggle="modal" data-target="#login-modal2">Registrarse</a>
 		<?php else: {	?>
 		<a href="#" class="navbar-brand" ><?php 	echo $_SESSION['name'];?>  </a>
-    <a href="#" class="navbar-brand" ><span class="glyphicon glyphicon-shopping-cart"> <span>Carrito</span></a>
-
+        <a href="#" class="navbar-brand" ><span class="glyphicon glyphicon-shopping-cart"></a>
 	<?php
 		}
 			?>
-
+		
 			<?php endif;?>
-
+		
       </div>
       <div class="collapse navbar-collapse" id="Barra-Navegacion">
         <ul class="nav navbar-nav">
@@ -64,18 +76,19 @@
 
           <li><a href="#"><span class="glyphicon glyphicon-envelope"></span> Contacto</a></li>
           <li><a href="#"><span class="glyphicon glyphicon-sunglasses"></span> Acerca de nosotros</a></li>
-
-		<?php 	if(!isset($_SESSION['name'])):
-
+        
+		<?php 	if(!isset($_SESSION['name'])): 
+			
 			?>
+		<li><a href="#" data-toggle="modal" data-target="#login-modal">      </a></li>
         <?php else: {	?>
-		<li><a href="../funciones/CerrarSesion.php" >Logout</a></li>
+		<li><a href="../../html-php/funciones/CerrarSesion.php" >Logout</a></li>
 		<?php
 		}
 			?>
 			<?php endif;?>
-
-
+			
+			
 		</ul>
       </div>
     </div>
@@ -88,29 +101,29 @@
 
 <div class="modal fade" id="login-modal" tabindex="-1" role="dialog" aria-hidden="true" style="display: none;">
     	  <div class="modal-dialog">
-
+				
 				<div class="loginmodal-container">
 					<h1>Iniciar sesion</h1><br>
-
-
-
-				 <form name="registrar" method="POST"  action="../funciones/ExistenciaUsuario.php"></br>
-
+				  
+				  
+				  
+				 <form name="ingresar" method="POST"  action="../../html-php/funciones/ExistenciaUsuario.php"></br>
+		
 		<input type="text" class="text" id="usuario" name="usuario" tabindex="4" placeholder="Usuario" minlength="5" maxlength="20" required><br>		<br>
 				<input type="password" class="text"  id="pass" name="pass" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Debe contener al menos un numero una Letra mayuscula una minuscula, y minimo 8 o más caracteres"  placeholder="Contraseña 1" required><br><br>
 
 					<input type="submit" name="login" class="login loginmodal-submit" value="Iniciar sesion">
 				  </form>
 
-
-
-
+				  
+				  
+				  
 				  <div class="login-help">
 					<a href="#" data-toggle="modal" data-target="#login-modal2">Registrar una nueva cuenta</a>
 				  </div>
 				</div>
-
-
+				
+				
 			</div>
 		  </div>
 
@@ -118,12 +131,71 @@
           	  <div class="modal-dialog">
       				<div class="loginmodal-container">
       					<h1>Registrar una nueva cuenta</h1><br>
-      				  <form>
-      					<input type="text" name="user" placeholder="Username">
-      					<input type="password" name="pass" placeholder="Password">
+      			
+
+
+
+
+				<form name="registrar" method="POST" onsubmit="return validacion()" action="../../html-php/funciones/envio_registro.php">
+      					
+		<input type="text" class="text" id="usuario" name="usuario" tabindex="4" placeholder="Usuario" minlength="5" maxlength="20" required>
+      	
+				<input type="password" class="text"  id="pass" name="pass" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Debe contener al menos un numero una Letra mayuscula una minuscula, y minimo 8 o más caracteres"  placeholder="Contraseña " required>
+				
+				<input type="password" class="text"  id="pass2" name="pass2" tabindex="8" placeholder="Confirmar Contraseña" required>
+				
+	<input type="text" class="text" id="nombre" name="nombre" tabindex="2" placeholder="Nombre(s)" maxlength="30" required>
+
+
+				<input type="text" class="text" id="apellido" name="apellido" tabindex="3" placeholder="Apellido(s)" maxlength="30" required>
+
+
+
+		<input type="email" class="text"  style='margin-top:10px;' id="email" name="email" tabindex="1" placeholder="Email" pattern="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[a-z]{2,3}$" required><br><br>
+
+		
+		
+		
+
+		
+		
+			<div id="demo" style="width:600px;">
+	<div id="demoIzq"><?php  generaPaises() ; ?></div>			
+			<div id="demoDer">
+					<select disabled="disabled" name="estados" id="estados">
+						<option value="0">Seleccionas opci&oacute;n...</option>
+					</select>
+				</div>
+			
+			
+			
+			
+			</div>
+		
+<input type="text" class="text" id="calle" name="calle" tabindex="4" placeholder="Colonia-calle" minlength="5" maxlength="30" required><br><br>			
+		
+		
+	
+
+<select  name="tarjeta" id="tarjeta" style='margin-bottom:20px;'><br>
+						<option value="1">American Express</option><br>
+						<option value="2">Visa</option><br>
+						<option value="3">Master Card</option><br>
+					</select><br>
+
+	<input type="text" name="TarjetaN" id="TarjetaN" placeholder="Numero Tarjeta" pattern="^(?:5[1-9][0-9]{2}|222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720)[0-9]{12}$|^3[47][0-9]{13}$|^4[0-9]{12}(?:[0-9]{3})?$" required> 				
+<input type="text" class="text" id="Titular" name="Titular" tabindex="2" placeholder="Titular Tarjeta" maxlength="30" required><br><br>	
+
+
+
       					<input type="submit" name="login" class="login loginmodal-submit" value="Registrarse">
       				  </form>
-      				</div>
+
+
+
+
+
+					  </div>
       			</div>
       		  </div>
 
@@ -138,7 +210,7 @@
           <div class="item active">
             <img src="../images/prueba1.jpg" class="img-responsive" alt="" width="100%"/>
             <div class="carousel-caption">
-              <h3>Siempre lo más fresco</h3>
+              <h3>Siempre lo mas fresco</h3>
               <p>
                 En TACOrporation nos preocupamos por que tus alimentos lleguen frescos a tu cocina.
               </p>
@@ -189,22 +261,29 @@
     <div class="thumbnail">
       <img src="http://placehold.it/500x300" alt="">
         <div class="caption">
-          <h2>TU GFA</h2>
-          <h3>$4000</h3>
-          <p>asa asa asa asa asa</p>
-          <?php 	if(!isset($_SESSION['name'])):
-
-            ?>
-              <?php else: {	?>
-
-                <a href="#" class="btn btn-info" role="button"><span class="glyphicon glyphicon-plus-sign"></span><span class="glyphicon glyphicon-shopping-cart"></span></a>
-                <?php
-                  }
-                ?>
-
-                <?php endif;?>
-
-      </div>
+          <h2>PRODUCTO</h2>
+          <h2>PRECIO</h2>
+          <p>DESCRIPCION BIEN PINSHE SHIDORINA ALV</p>
+		
+		 
+			<?php 	if(!isset($_SESSION['name'])): 
+			
+			?>
+          <a href="#" class="btn btn-info btn-xs" data-toggle="modal" data-target="#login-modal" role="button"><span class="glyphicon glyphicon-plus-sign"></span><span class="glyphicon glyphicon-shopping-cart"></span></a>
+<?php else: {	?>
+	 <a href="#" class="btn btn-info btn-xs"  role="button"><span class="glyphicon glyphicon-plus-sign"></span><span class="glyphicon glyphicon-shopping-cart"></span></a>
+	
+<input type="number" style='width:80px;' class="cantidad" id="cantidad" name="cantidad" tabindex="4" placeholder="Cant" required><br><br>			
+		
+	<?php
+		}
+		
+			?>
+			<?php endif;?>	  
+		  
+		  
+		  
+		  </div>
     </div>
   </div>
 
